7f187220a53d8b6109d8bca0a8ceed18d2bee197
substack <substack@9nTgtYmvW4HID6ayt6Icwc8WZxdifx5SlSKKIX/X/1g=.ed25519>
2017-10-04 23:41:01 -1000

    fix parser offsets

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ module.exports = function (abuf) {
       break
     }
   }
+  var offsets = { attributes: i+1 }
   var lines = encode(data.subarray(0,i-1)).split('\n')
   var m = /^BGA (.+)/.exec(lines[0])
   if (!m) throw new Error('magic number not found')
@@ -24,12 +25,20 @@ module.exports = function (abuf) {
     types: { edge: 'uint16', triangle: 'uint16' },
     counts: { edge: 0, triangle: 0 }
   }
+  var stride = 0
   for (var i = 1; i < lines.length; i++) {
     var line = lines[i]
     if (m = /^(little|big) endian/.exec(line)) {
       header.endian = m[1]
     } else if (m = /^attribute (\w+) (\w+)$/.exec(line)) {
-      header.attributes.push({ name: m[2], type: m[1] })
+      var name = m[2], type = m[1]
+      var size = sizes[type]
+      header.attributes.push({
+        name: name,
+        type: type,
+        size: size
+      })
+      stride += size
     } else if (m = /^(\d+) vertex$/.exec(line)) {
       header.counts.vertex = Number(m[1])
     } else if (m = /^(\d+) (edge|triangle) (uint16|uint32)$/.exec(line)) {
@@ -37,13 +46,19 @@ module.exports = function (abuf) {
       header.types[m[2]] = m[3]
     }
   }
+  var attrOffset = 0
+  for (var i = 0; i < header.attributes.length; i++) {
+    var attr = header.attributes[i]
+    attr.offset = attrOffset
+    attrOffset += attr.size
+    attr.stride = stride
+  }
   var vsize = 0
   for (var i = 0; i < header.attributes.length; i++) {
     vsize += sizes[header.attributes[i].type]
   }
-  var offsets = { attributes: i }
   var lengths = {
-    vertex: header.counts.vertex * vsize,
+    attributes: header.counts.vertex * vsize,
     edges: header.counts.edge * sizes[header.types.edge],
     triangles: header.counts.triangle * sizes[header.types.triangle]
   }
@@ -52,9 +67,23 @@ module.exports = function (abuf) {
   return {
     header: header,
     data: {
-      attributes: data.subarray(offsets.attributes, lengths.attributes),
-      edges: data.subarray(offsets.edges, lengths.edges),
-      triangles: data.subarray(offsets.triangles, lengths.triangles)
+      attributes: data.subarray(
+        offsets.attributes, offsets.attributes + lengths.attributes),
+      edges: header.types.edge
+        ? toarray(header.types.edge,
+            data.subarray(offsets.edges, offsets.edges + lengths.edges))
+        : [],
+      triangles: header.types.triangle
+        ? toarray(header.types.triangle,
+            data.subarray(offsets.triangles,
+              offsets.triangles + lengths.triangles))
+        : []
     }
   }
+}
+
+function toarray (type, data) {
+  if (type === 'uint16') return new Uint16Array(data.buffer)
+  if (type === 'uint32') return new Uint32Array(data.buffer)
+  throw new Error('unexpected type ' + type)
 }
